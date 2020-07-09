557bb2d20db94643d0e2538875a19d62809d876d
Robin177 <39666335+Robin177@users.noreply.github.com>
2020-07-09 08:49:42 +0800

    Update and rename test_3(正确只有一张错误截图).py to test_3.py

--- UnitTest/test_3.py
+++ UnitTest/test_3.py
@@ -36,15 +36,6 @@ class Test_BaiDu_SouSuo(unittest.TestCase):
         # 断言判断lDemon_百度搜索等于标题
         self.assertEqual("Demon_百度搜索",self.driver.title)
 
-    def test_003(self):
-
-        self.driver.find_element_by_id("kw").send_keys("laohu")
-        self.driver.find_element_by_id("su").click()
-        time.sleep(2)
-        # 断言判断laohu在源码里面
-        # self.assertIn("laohu",self.driver.page_source)
-        # 断言判断laohu等于源码
-        self.assertEqual("laohu",self.driver.page_source)
 
 if __name__ == "__main__":
     unittest.main()# 调用main方法执行unitetest内所有test开头方法
